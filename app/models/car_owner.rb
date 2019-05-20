cb9d3faf629383eabfd4a5e2336c1a2a25fcb023
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-05-20 14:52:24 +0100

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,26 @@
 class CarOwner
 
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  def cars
+    Car.all.select {|car| car.owner == self}
+  end
+
+  def mechanics
+    cars.map { |car| car.mechanic}
+  end
+
+  def self.avgCar
+    (self.all.count.to_f / Car.all.count.to_f)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,16 @@ def reload
   load 'config/environment.rb'
 end
 
+OwnerOne = CarOwner.new("Jack")
+OwnerTwo = CarOwner.new("Joseph")
+OwnerThree = CarOwner.new("James")
+
+MechanicOne = Mechanic.new("Seb", "Motorbikes")
+MechanicTwo = Mechanic.new("Tel", "Racing")
+MechanicThree = Mechanic.new("Reb", "Comfort")
+
+CarOne = Car.new("Audi","R5","Comfort", OwnerOne, MechanicThree)
+CarTwo = Car.new("Audi","R4","Racing", OwnerOne, MechanicTwo)
+CarThree = Car.new("Audi","R3","Racing", OwnerTwo, MechanicTwo)
+CarFour = Car.new("Audi","R1","Motorbikes", OwnerThree, MechanicOne)
 binding.pry
