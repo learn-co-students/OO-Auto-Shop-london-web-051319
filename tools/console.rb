2c108cff396e871d87a2c2c53059a1da22966cd3
Daniel Bovington <daniel.bovington@flatironschool.com>
2019-05-20 13:24:54 +0100

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,17 @@ def reload
   load 'config/environment.rb'
 end
 
+dan = CarOwner.new("Dan")
+bill = CarOwner.new("Bill")
+bob = CarOwner.new("Bob")
+
+todd = Mechanic.new("Todd", "Sports")
+chris = Mechanic.new("Chris", "Hatchback")
+aiden = Mechanic.new("Aiden", "Sports")
+
+
+bmw = Car.new("BMW", "M3", "Sports", dan, todd)
+vw = Car.new("VW", "Golf", "Hatchback", bill, chris)
+fiat = Car.new("Fiat", "Punto", "Hatchback", bob, aiden)
+
 binding.pry
